f9937dbfbde0e5319e31cc037a6782a313c46dd3
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-03-03 14:06:50 -0800

    Added Character struct to render_system.hpp

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -13,6 +13,15 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 #include <map>
+
+struct Character {
+	unsigned int TextureID;  // ID handle of the glyph texture
+	glm::ivec2   Size;       // Size of glyph
+	glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+	unsigned int Advance;    // Offset to advance to next glyph
+	char character;
+};
+
 // System responsible for setting up OpenGL and for rendering all the
 // visual entities in the game
 class RenderSystem {
