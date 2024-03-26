ae9e12257fc7fec993e82bb4f7b45f36e25a11e0
Kush Sethi <kushagra.sethi.io@gmail.com>
2024-03-26 12:29:20 -0700

    Added particles fragment and vertex shaders

---
+++ shaders/particles.fs.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+
+in float lifetime;
+
+out vec4 fragColor;
+
+void main()
+{
+   fragColor = vec4( 1.0, 0.0, 0.0, 1.0);
+}
---
+++ shaders/particles.vs.glsl
@@ -0,0 +1,18 @@
+#version 330 core
+
+layout ( location = 0 ) in vec3 vertex_position;
+layout ( location = 4 ) in vec4 position;
+
+uniform mat3 M_v;
+uniform mat3 M_p;
+uniform float particleSize;
+
+out float lifetime;
+
+void main()
+{
+   vec3 position_viewspace = M_v * position.xyz;
+   position_viewspace.xy += particleSize * (vertex_position.xy - vec2(0.5));
+   gl_Position = vec4(M_p * position_viewspace, 1.0);
+   lifetime = position.w;
+};
