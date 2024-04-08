f01b59517ef613a98c8511338772a864267537a2
UBC Student <sheunged@students.cs.ubc.ca>
2024-04-08 13:16:15 -0700

    Revert "Merge final"

--- src/world_init.cpp
+++ src/world_init.cpp
@@ -84,35 +84,6 @@ Entity createHealthBar(RenderSystem* renderer, vec2 pos, vec2 bounds)
 	return entity;
 }
 
-Entity create_shield_health(RenderSystem* renderer, const vec2& position, const vec2& bounds)
-{
-	auto entity = Entity();
-
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::SPRITE);
-	registry.meshPtrs.emplace(entity, &mesh);
-
-	// Initialize the motion
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0, 0 };
-	motion.position = position;
-	motion.scale = bounds;
-	
-	registry.shields.emplace(entity);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::SHIELD_POWERUP,
-			TEXTURE_ASSET_ID::TEXTURE_COUNT,
-		 EFFECT_ASSET_ID::TEXTURED,
-		 GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-
-
 
 Entity createBullet(RenderSystem* renderer, vec2 pos, vec2 velocity, float angle) {
 	auto entity = Entity();
@@ -229,6 +200,12 @@ Entity create_directional_light(RenderSystem* renderer, const vec2& position, co
 
 	renderer->setDirectionalLight(entity);
 
+	registry.renderRequests.insert(
+		entity,
+		{ TEXTURE_ASSET_ID::DIRECTIONAL_LIGHT,
+			TEXTURE_ASSET_ID::TEXTURE_COUNT,
+		 EFFECT_ASSET_ID::TEXTURED,
+		 GEOMETRY_BUFFER_ID::SPRITE });
 
 	return entity;
 }
@@ -471,105 +448,6 @@ Entity create_lemon_powerup(RenderSystem* renderer, const vec2& position, const 
 	return entity;
 }
 
-Entity create_cherry_powerup(RenderSystem* renderer, const vec2& position, const vec2& bounds)
-{
-	auto entity = Entity();
-
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::MINION);
-	registry.meshPtrs.emplace(entity, &mesh);
-	registry.mesh_collision.emplace(entity);
-
-	// Initialize the motion
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0, 0 };
-	motion.position = position;
-
-	// Setting initial values, scale is negative to make it face the opposite way
-	motion.scale = vec2({ -bounds.x, bounds.y });
-	// Create and (empty) powerup component to be able to refer to all minions
-	auto& powerup = registry.powerUps.emplace(entity);
-	powerup.type = POWERUP_TYPE::CHERRY;
-
-	create_mesh(renderer, motion.position, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::LEMON_POWERUP, TEXTURE_ASSET_ID::TEXTURE_COUNT, GEOMETRY_BUFFER_ID::LEMON, false);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::CHERRY_POWERUP,
-			TEXTURE_ASSET_ID::TEXTURE_COUNT,
-		 EFFECT_ASSET_ID::TEXTURED,
-		 GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-Entity create_shield_powerup(RenderSystem* renderer, const vec2& position, const vec2& bounds)
-{
-	auto entity = Entity();
-
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::MINION);
-	registry.meshPtrs.emplace(entity, &mesh);
-	registry.mesh_collision.emplace(entity);
-
-	// Initialize the motion
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0, 0 };
-	motion.position = position;
-
-	// Setting initial values, scale is negative to make it face the opposite way
-	motion.scale = vec2({ -bounds.x, bounds.y });
-	// Create and (empty) powerup component to be able to refer to all minions
-	auto& powerup = registry.powerUps.emplace(entity);
-	powerup.type = POWERUP_TYPE::SHIELD;
-	create_mesh(renderer, motion.position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::BATTERY_POWERUP, TEXTURE_ASSET_ID::BATTERY_POWERUP_NM, GEOMETRY_BUFFER_ID::BATTERY, false);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::SHIELD_POWERUP,
-			TEXTURE_ASSET_ID::TEXTURE_COUNT,
-		 EFFECT_ASSET_ID::TEXTURED,
-		 GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-Entity create_cactus_powerup(RenderSystem* renderer, const vec2& position, const vec2& bounds)
-{
-	auto entity = Entity();
-
-	// Store a reference to the potentially re-used mesh object (the value is stored in the resource cache)
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::MINION);
-	registry.meshPtrs.emplace(entity, &mesh);
-	registry.mesh_collision.emplace(entity);
-
-	// Initialize the motion
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0, 0 };
-	motion.position = position;
-
-	// Setting initial values, scale is negative to make it face the opposite way
-	motion.scale = vec2({ -bounds.x, bounds.y });
-	// Create and (empty) powerup component to be able to refer to all minions
-	auto& powerup = registry.powerUps.emplace(entity);
-	powerup.type = POWERUP_TYPE::CACTUS;
-	create_mesh(renderer, motion.position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::BATTERY_POWERUP, TEXTURE_ASSET_ID::BATTERY_POWERUP_NM, GEOMETRY_BUFFER_ID::BATTERY, false);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::CACTUS_POWERUP,
-			TEXTURE_ASSET_ID::TEXTURE_COUNT,
-		 EFFECT_ASSET_ID::TEXTURED,
-		 GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-
-
 Entity create_lemon_bullet(RenderSystem* renderer, vec2 pos, vec2 velocity, float angle) {
 	auto entity = Entity();
 	// Store a reference to the potentially re-used mesh object, like createChicken
@@ -626,33 +504,6 @@ Entity create_fast_bullet(RenderSystem* renderer, vec2 pos, vec2 velocity, float
 	return entity;
 }
 
-Entity create_cactus_bullet(RenderSystem* renderer, vec2 pos, vec2 velocity, float angle) {
-	auto entity = Entity();
-	// Store a reference to the potentially re-used mesh object, like createChicken
-	//Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::BULLET);
-	//registry.meshPtrs.emplace(entity, &mesh);
-
-	registry.mesh_collision.emplace(entity);
-	Motion& motion = registry.motions.emplace(entity);
-	motion.position = pos;
-	motion.angle = angle;
-	motion.velocity = velocity;
-	motion.scale = vec2(60.f, 100.0f);
-	auto& bullet = registry.bullets.emplace(entity);
-
-	vec3 color = { 0, 1, 0 };
-	registry.colors.insert(entity, color);
-	create_mesh(renderer, pos, velocity, { 0.4 * motion.scale.x, 0.8 * motion.scale.y }, angle, entity, TEXTURE_ASSET_ID::BULLET, TEXTURE_ASSET_ID::BULLET_NM, GEOMETRY_BUFFER_ID::BULLET, false);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::BULLET,
-			TEXTURE_ASSET_ID::BULLET_NM,
-		 EFFECT_ASSET_ID::TEXTURED,
-		 GEOMETRY_BUFFER_ID::SPRITE });
-	return entity;
-}
-
 
 
 Entity create_dodger(RenderSystem* renderer, const vec2& position, const vec2& bounds) {
@@ -670,10 +521,10 @@ Entity create_dodger(RenderSystem* renderer, const vec2& position, const vec2& b
 	motion.velocity = { 0, -80.f };
 	motion.position = position;
 	motion.scale = vec2({ -bounds.x, bounds.y});
-
-	/*vec3 color = { 0.8,0.8,0.0 };
-	registry.colors.insert(entity, color);*/
+	vec3 color = { 0.8,0.8,0.0 };
+	registry.colors.insert(entity, color);
 	create_mesh(renderer, position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::MINION, TEXTURE_ASSET_ID::MINION_NM, GEOMETRY_BUFFER_ID::MINION, false);
+
 
 	auto& dodger = registry.shooters.emplace(entity);
 
@@ -709,39 +560,6 @@ Entity create_charger(RenderSystem* renderer, const vec2& position, const vec2& 
 	create_mesh(renderer, position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::MINION, TEXTURE_ASSET_ID::MINION_NM, GEOMETRY_BUFFER_ID::MINION, false);
 
 	auto& charger = registry.chargers.emplace(entity);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::MINION,
-		  TEXTURE_ASSET_ID::MINION_NM,
-		  EFFECT_ASSET_ID::TEXTURED,
-		  GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-Entity create_boss(RenderSystem* renderer, const vec2& position, const vec2& bounds) {
-	auto entity = Entity();
-
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::MINION);
-	registry.mesh_collision.emplace(entity);
-	registry.meshPtrs.emplace(entity, &mesh);
-	auto& motion = registry.motions.emplace(entity);
-	auto& minion = registry.minions.emplace(entity);
-	auto& boss = registry.boss.emplace(entity);
-	minion.type = Enemy_TYPE::BOSS;
-	minion.score = 10000;
-	motion.angle = 0.f;
-	minion.health = 4000;
-	minion.max_health = 4000;
-	minion.armor = 0;
-	minion.speed = 200;
-	motion.velocity = { 0, -80.f };
-	motion.position = position;
-	motion.scale = vec2({ -bounds.x, bounds.y });
-	vec3 color = { 0.5,0.2,0.2 };
-	registry.colors.insert(entity, color);
-
 
 	registry.renderRequests.insert(
 		entity,
@@ -804,10 +622,10 @@ Entity create_sniper(RenderSystem* renderer, const vec2& position, const vec2& b
 	minion.max_health = 50;
 	motion.position = position;
 	motion.scale = vec2({ -bounds.x, bounds.y });
-
-	/*vec3 color = { 0.2,0.8,0.8 };
-	registry.colors.insert(entity, color);*/
+	vec3 color = { 0.2,0.8,0.8 };
+	registry.colors.insert(entity, color);
 	create_mesh(renderer, position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::MINION, TEXTURE_ASSET_ID::MINION_NM, GEOMETRY_BUFFER_ID::MINION, false);
+
 	auto& dodger = registry.snipers.emplace(entity);
 
 	registry.renderRequests.insert(
@@ -849,57 +667,12 @@ Entity create_roamer(RenderSystem* renderer, const vec2& position, const vec2& b
 
 	motion.position = position;
 	motion.scale = vec2({ -bounds.x, bounds.y });
-	/*vec3 color = { 0,1,0 };
-	registry.colors.insert(entity, color);*/
+	vec3 color = { 0,1,0 };
+	registry.colors.insert(entity, color);
 
 	create_mesh(renderer, position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::MINION, TEXTURE_ASSET_ID::MINION_NM, GEOMETRY_BUFFER_ID::MINION, false);
 
 	auto& dodger = registry.roamers.emplace(entity);
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::MINION,
-		  TEXTURE_ASSET_ID::MINION_NM,
-		  EFFECT_ASSET_ID::TEXTURED,
-		GEOMETRY_BUFFER_ID::SPRITE });
-
-	return entity;
-}
-
-Entity create_cleaner(RenderSystem* renderer, const vec2& position, const vec2& bounds)
-{
-	auto entity = Entity();
-
-
-	Mesh& mesh = renderer->getMesh(GEOMETRY_BUFFER_ID::MINION);
-	registry.mesh_collision.emplace(entity);
-	registry.meshPtrs.emplace(entity, &mesh);
-	auto& motion = registry.motions.emplace(entity);
-	auto& minion = registry.minions.emplace(entity);
-	minion.type = Enemy_TYPE::CLEANER;
-	minion.score = 50;
-	minion.health = 110;
-	minion.speed = 70;
-	motion.angle = 0.f;
-
-	std::random_device rd;
-	std::mt19937 eng(rd());
-	std::uniform_real_distribution<> distr(60, 70);
-	std::uniform_int_distribution<> signDistr(0, 1);
-
-	float velocityX = distr(eng) * (signDistr(eng) * 2 - 1);
-	float velocityY = distr(eng) * (signDistr(eng) * 2 - 1);
-
-	motion.velocity = { velocityX, velocityY };
-
-	motion.position = position;
-	motion.scale = vec2({ -bounds.x, bounds.y });
-	vec3 color = { 0.8,0.8,0.0 };
-	registry.colors.insert(entity, color);
-
-	registry.roamers.emplace(entity);
-	registry.cleaners.emplace(entity);
-	create_mesh(renderer, motion.position, motion.velocity, motion.scale, motion.angle, entity, TEXTURE_ASSET_ID::BATTERY_POWERUP, TEXTURE_ASSET_ID::BATTERY_POWERUP_NM, GEOMETRY_BUFFER_ID::BATTERY, false);
 
 	registry.renderRequests.insert(
 		entity,
@@ -965,7 +738,6 @@ Entity create_mesh(RenderSystem* renderer, vec2 pos, vec2 velocity, vec2 scale, 
 	return entity;
 }
 
-
 Entity create_mesh(RenderSystem* renderer, vec2 pos, vec2 scale, float angle, Entity object_entity, TEXTURE_ASSET_ID texture_id_one, TEXTURE_ASSET_ID texture_id_two, GEOMETRY_BUFFER_ID geometry_id, bool display) {
 	auto entity = Entity();
 	Mesh& mesh = renderer->getMesh(geometry_id);
@@ -984,26 +756,5 @@ Entity create_mesh(RenderSystem* renderer, vec2 pos, vec2 scale, float angle, En
 			 EFFECT_ASSET_ID::CHICKEN,
 			geometry_id });
 	}
-  	return entity;
-}
-
-Entity create_cursor(RenderSystem* renderer, const vec2& position)
-{
-	auto entity = Entity();
-	registry.cursor.emplace(entity);
-	auto& motion = registry.motions.emplace(entity);
-	motion.angle = 0.f;
-	motion.velocity = { 0,0 };
-	motion.position = position;
-	motion.scale = vec2({ 50,50 });
-
-	registry.renderRequests.insert(
-		entity,
-		{ TEXTURE_ASSET_ID::CURSOR,
-			TEXTURE_ASSET_ID::TEXTURE_COUNT,
-		 EFFECT_ASSET_ID::TEXTURED,
-		GEOMETRY_BUFFER_ID::SPRITE });
-
-
 	return entity;
 }
